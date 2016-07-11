59b12cd77d950172b4375871992d2781bffb6d69
kkaluga <orsha.kaluga@gmail.com>
2016-07-12 00:58:26 +0300

    start tests and logs

---
+++ library/src/test/java/by/tr/library/dao/impl/file/FileAdminDaoTest.java
@@ -0,0 +1,36 @@
+package by.tr.library.dao.impl.file;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kirill Kaluga on 12.07.2016.
+ */
+public class FileAdminDaoTest {
+    @Test
+    public void blockUser() throws Exception {
+
+    }
+
+    @Test
+    public void unBlockUser() throws Exception {
+
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+
+    }
+
+    @Test
+    public void addNewBook() throws Exception {
+
+    }
+
+    @Test
+    public void deleteBook() throws Exception {
+
+    }
+
+}
---
+++ library/src/test/java/by/tr/library/dao/impl/file/FileCommonDaoTest.java
@@ -0,0 +1,16 @@
+package by.tr.library.dao.impl.file;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kirill Kaluga on 12.07.2016.
+ */
+public class FileCommonDaoTest {
+    @Test
+    public void authorization() throws Exception {
+
+    }
+
+}
