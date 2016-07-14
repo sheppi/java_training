32e82282e933f28b00dd2228e9b6f7946de9f6c4
Kirill Kaluga <kirill_kaluga@epam.com>
2016-07-14 16:11:48 +0300

    progress at connection pool

--- pool/src/main/java/com/epam/pool/controller/ConnectionPool.java
+++ pool/src/main/java/com/epam/pool/controller/ConnectionPool.java
@@ -2,16 +2,16 @@ package com.epam.pool.controller;
 
 import com.epam.pool.entity.DBParameter;
 import com.epam.pool.exception.ConnectionPoolException;
-import com.sun.jndi.ldap.pool.PooledConnection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
 import java.util.Vector;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
 import java.util.logging.Level;
 
 /**
@@ -85,7 +85,342 @@ public class ConnectionPool {
         return connection;
     }
 
-    private void closeConnectionQueue(BlockingQueue<Connection> giveAwayConQueue) {
+    public void closeConnection(Connection con, Statement st, ResultSet rs) {
+        try{
+            con.close();
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
+
+        try{
+            rs.close();
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
+
+        try{
+            st.close();
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
     }
+
+    public void closeConnection(Connection con, Statement st){
+        try{
+            con.close();
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
+    }
+
+    private void closeConnectionQueue(BlockingQueue<Connection> queue) throws SQLException {
+        Connection connection;
+        while ((connection = queue.poll()) != null){
+            if (!connection.getAutoCommit()){
+                connection.commit();
+            }
+            ((PooledConnection) connection).reallyClose();
+        }
+    }
+
+    private class PooledConnection implements Connection {
+
+        private Connection connection;
+
+        public PooledConnection(Connection c) throws SQLException{
+            this.connection = c;
+            this.connection.setAutoCommit(true);
+        }
+
+        public void reallyClose() throws SQLException{
+            connection.close();
+        }
+
+        @Override
+        public Statement createStatement() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public PreparedStatement prepareStatement(String sql) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public CallableStatement prepareCall(String sql) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public String nativeSQL(String sql) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public void setAutoCommit(boolean autoCommit) throws SQLException {
+
+        }
+
+        @Override
+        public boolean getAutoCommit() throws SQLException {
+            return false;
+        }
+
+        @Override
+        public void commit() throws SQLException {
+
+        }
+
+        @Override
+        public void rollback() throws SQLException {
+
+        }
+
+        @Override
+        public void close() throws SQLException {
+            if(connection.isClosed()){
+                throw new SQLException("Attemting to close closed connection.");
+            }
+
+            if (connection.isReadOnly()){
+                connection.setReadOnly(false);
+            }
+
+            if (!giveAwayConQueue.remove(this)){
+                throw new SQLException("Error deleting connection from the given away connections pool.");
+            }
+
+            if (!connectionQueue.offer(this)){
+                throw new SQLException("Error allocating connection in the pool.");
+            }
+        }
+
+        @Override
+        public boolean isClosed() throws SQLException {
+            return false;
+        }
+
+        @Override
+        public DatabaseMetaData getMetaData() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public void setReadOnly(boolean readOnly) throws SQLException {
+
+        }
+
+        @Override
+        public boolean isReadOnly() throws SQLException {
+            return false;
+        }
+
+        @Override
+        public void setCatalog(String catalog) throws SQLException {
+
+        }
+
+        @Override
+        public String getCatalog() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public void setTransactionIsolation(int level) throws SQLException {
+
+        }
+
+        @Override
+        public int getTransactionIsolation() throws SQLException {
+            return 0;
+        }
+
+        @Override
+        public SQLWarning getWarnings() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public void clearWarnings() throws SQLException {
+            connection.clearWarnings();
+        }
+
+        @Override
+        public Statement createStatement(int resultSetType, int resultSetConcurrency) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public PreparedStatement prepareStatement(String sql, int resultSetType, int resultSetConcurrency) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public Map<String, Class<?>> getTypeMap() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
+
+        }
+
+        @Override
+        public void setHoldability(int holdability) throws SQLException {
+
+        }
+
+        @Override
+        public int getHoldability() throws SQLException {
+            return 0;
+        }
+
+        @Override
+        public Savepoint setSavepoint() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public Savepoint setSavepoint(String name) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public void rollback(Savepoint savepoint) throws SQLException {
+
+        }
+
+        @Override
+        public void releaseSavepoint(Savepoint savepoint) throws SQLException {
+
+        }
+
+        @Override
+        public Statement createStatement(int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public PreparedStatement prepareStatement(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public PreparedStatement prepareStatement(String sql, int autoGeneratedKeys) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public PreparedStatement prepareStatement(String sql, int[] columnIndexes) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public PreparedStatement prepareStatement(String sql, String[] columnNames) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public Clob createClob() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public Blob createBlob() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public NClob createNClob() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public SQLXML createSQLXML() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public boolean isValid(int timeout) throws SQLException {
+            return false;
+        }
+
+        @Override
+        public void setClientInfo(String name, String value) throws SQLClientInfoException {
+
+        }
+
+        @Override
+        public void setClientInfo(Properties properties) throws SQLClientInfoException {
+
+        }
+
+        @Override
+        public String getClientInfo(String name) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public Properties getClientInfo() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public Array createArrayOf(String typeName, Object[] elements) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public Struct createStruct(String typeName, Object[] attributes) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public void setSchema(String schema) throws SQLException {
+
+        }
+
+        @Override
+        public String getSchema() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public void abort(Executor executor) throws SQLException {
+
+        }
+
+        @Override
+        public void setNetworkTimeout(Executor executor, int milliseconds) throws SQLException {
+
+        }
+
+        @Override
+        public int getNetworkTimeout() throws SQLException {
+            return 0;
+        }
+
+        @Override
+        public <T> T unwrap(Class<T> iface) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public boolean isWrapperFor(Class<?> iface) throws SQLException {
+            return false;
+        }
+    }
+
+
 }
 
